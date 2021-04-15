6e1cff859aac4cc8136275e2af1e2c3b352e6693
sairatabassum <sairatabassum386@gmail.com>
2021-04-15 16:02:28 +0600

    commit

--- Program2/String.js
+++ Program2/String.js
@@ -1,10 +1,14 @@
 // String Concatenation
 document.write("<br>Saira "+ "Tabassum<br>")
 
-var num1=10
+var num1=80
 var num2=30
 
-document.write("Num1= "+num1+" Num2= "+num2)
+document.write("Num1= "+num1+" Num2= "+num2+"<br>")
 console.log(typeof(num1))
 
-// 
+// String Length
+
+var name="Saira Tabassum";
+document.write("Length of string= "+name.length);
+
