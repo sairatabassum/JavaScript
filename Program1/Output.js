923ad9c4e69f443161da384af61ba031131399cf
sairatabassum <sairatabassum386@gmail.com>
2021-04-15 15:26:12 +0600

    commit

---
+++ Program1/Output.js
@@ -0,0 +1,60 @@
+
+// Single comment
+//document.write("Saira Tabassum <br>");
+
+// Multiple Comment
+/*
+document.write("01198273213 <br>");
+document.write("Rajshahi")
+*/
+
+document.write("<h1>Happy Ramadan</h1>")
+
+// Variable
+
+var name="Saira Tabassum"
+document.write(name+"<br>");
+
+var Name,Age;
+Name="Shufia"
+Age=32
+document.write(Name+"<br>"+Age)
+
+// Number to String
+var num=12
+var str=toString(num)
+console.log(typeof(str));
+
+// String to IntNumber
+var str="123"
+var num=parseInt(str)
+console.log(typeof(num))
+
+// String to Float
+var str="12.78"
+var flt=parseFloat(str)
+console.log(typeof(flt))
+
+// Show the decimal point
+var flt=12.3534634
+console.log(flt.toFixed(2))
+
+// Show the number by length size
+var flt=12.324324
+console.log(flt.toPrecision(2))
+
+// Number Function
+console.log(Number("12"))
+console.log(Number(true))
+console.log(Number(false))
+
+// String Concatenation
+document.write("<br>Saira "+ "Tabassum<br>")
+
+var num1=10
+var num2=30
+
+document.write("Num1= "+num1+" Num2= "+num2)
+console.log(typeof(num1))
+
+// 
