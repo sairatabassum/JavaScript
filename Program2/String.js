5f785df29a440c48eb761d8ec603ef451b6a6728
sairatabassum <sairatabassum386@gmail.com>
2021-04-15 17:12:28 +0600

    commit

--- Program2/String.js
+++ Program2/String.js
@@ -7,6 +7,14 @@ var num2=30
 document.write("Num1= "+num1+" Num2= "+num2+"<br>")
 console.log(typeof(num1))
 
+// ConCat funtion
+var nam1="Saira ";
+var nam2="Tabassum";
+
+var fullname=nam1.concat(nam2)
+document.write(fullname+"<br>")
+
+
 // String Length
 
 var name="Saira Tabassum";
@@ -26,6 +34,12 @@ document.write(name.charAt(1)+"<br>");
 
 // UpperCase  & LowerCase
 
-var name="Saira Tabassum"
-document.write(name.toUpperCase()+"<br>")
-document.write(name.toLowerCase()+"<br>")
+var name="Saira Tabassum";
+document.write(name.toUpperCase()+"<br>");
+document.write(name.toLowerCase()+"<br>");
+
+// Slice function
+
+var nam="Saira Tabassum";
+var result=name.slice(0,5);
+document.write(result);
