6455fafdef7876b506e8229e88ab73b0b3d7b676
sairatabassum <sairatabassum386@gmail.com>
2021-04-16 17:07:24 +0600

    commit

--- Program3/Arithmetic/Sum.js
+++ Program3/Arithmetic/Sum.js
@@ -6,4 +6,10 @@ num2=parseInt(num2,10);
 
 var sum=num1+num2;
 
-document.write(sum)
+num3=prompt("Input number3: ");
+num4=prompt("Input number4: ");
+
+var res=parseFloat(num3)+parseFloat(num4);
+
+document.write(sum+"<br>")
+document.write(res);
