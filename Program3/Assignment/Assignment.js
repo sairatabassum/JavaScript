036f875f65eb05eef68ce3c0263422aa8c5eb87d
sairatabassum <sairatabassum386@gmail.com>
2021-04-16 15:16:05 +0600

    commit

--- Program3/Assignment/Assignment.js
+++ Program3/Assignment/Assignment.js
@@ -1,2 +1,22 @@
 // =, += , -= ,*= , /= , %= , **=
 
+var x=5;
+x=Number(x);
+
+x+=6;
+document.write(  x +"<br>");
+
+x-=6
+document.write(  x +"<br>");
+
+x*=6
+document.write(  x +"<br>");
+
+x/=6
+document.write(  x +"<br>");
+
+x%=6
+document.write(  x +"<br>");
+
+x**=6
+document.write(  x +"<br>");
