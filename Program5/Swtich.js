31552e0da7b89c05cc7b31fcade2324d46711099
sairatabassum <sairatabassum386@gmail.com>
2021-04-17 02:20:14 +0600

    commit

--- Program5/Swtich.js
+++ Program5/Swtich.js
@@ -1,4 +1,54 @@
 
+// switch, case, break, default
 
-var digit=propmt("Enter a digit: ");
+var digit=("Enter a digit: ");
+document.write(typeof(digit)+"<br>");
 
+switch(digit){
+
+    case "0":
+        document.write("zero");
+        break;
+
+    case "1":
+        document.write("one");
+        break;
+
+    case "2":
+        document.write("two");
+        break;
+
+    case "3":
+        document.write("three");
+        break;
+
+    case "4":
+        document.write("four");
+        break;
+
+    case "5":
+        document.write("five");
+        break;
+
+    case "6":
+        document.write("six");
+        break;
+
+    case "7":
+        document.write("seven");
+        break;
+
+    case "8":
+        document.write("eight");
+        break;
+
+    case "9":
+        document.write("nine");
+        break;
+
+    default:
+        document.write("not digit");
+
+
+
+}
