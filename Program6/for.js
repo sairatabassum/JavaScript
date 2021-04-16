d2e8bdca859365ee8287161a2d1b9483bd0bfa9f
sairatabassum <sairatabassum386@gmail.com>
2021-04-17 02:49:38 +0600

    commit

---
+++ Program6/for.js
@@ -0,0 +1,18 @@
+
+
+for(var i=1; i<=10; i++){
+
+    document.write("<h1>JS</h1><br>");
+}
+
+
+for(var i=10; i>=0; i--){
+
+    document.write("JS<br>");
+}
+
+
+for(var i=1; i<=10; i+=2){
+
+    document.write(i+",");
+}
