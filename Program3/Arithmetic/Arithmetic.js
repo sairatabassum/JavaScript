7318bf5a20b467a5d905d839d918c1dbcdb0249a
sairatabassum <sairatabassum386@gmail.com>
2021-04-16 14:51:12 +0600

    commit

---
+++ Program3/Arithmetic/Arithmetic.js
@@ -0,0 +1,13 @@
+// + ,- , * ,/  ,%  . **(Exponent)
+
+document.write(7+2+"<br>");
+
+document.write(12-3+"<br>");
+
+document.write(6*6+"<br>");
+
+document.write(6/6+"<br>");
+
+document.write(12%3+"<br>");
+
+document.write(2**3+"<br>");
