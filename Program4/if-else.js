b97dc39500c5501da33972b424fe71e187452f02
sairatabassum <sairatabassum386@gmail.com>
2021-04-17 01:16:43 +0600

    commit

--- Program4/if-else.js
+++ Program4/if-else.js
@@ -1,7 +1,20 @@
 var x=10;
 
 if (x%2==0)
-document.write("Even");
+document.write("Even<br>");
 
 else
-document.write("Odd");
+document.write("Odd<br>");
+
+// if,else if ,else
+
+var y=10;
+
+if(y>0)
+document.write("Positive<br>");
+
+else if(y<0)
+document.write("Negative<br>");
+
+else
+document.write("Zero<br>");
