17b038e0100ca8a83a3184f432c040055ef88d08
sairatabassum <sairatabassum386@gmail.com>
2021-04-17 02:29:22 +0600

    commit

---
+++ Program5/Vowel.js
@@ -0,0 +1,18 @@
+
+
+var char=prompt("Enter character: ");
+char=char.toLowerCase();
+
+switch(char){
+
+    case 'a':
+    case 'i':
+    case 'o':
+    case 'e':
+    case 'u':
+        document.write("<br> Vowel");
+        break;
+    default:
+        document.write("<br> Consonant");
+
+}
