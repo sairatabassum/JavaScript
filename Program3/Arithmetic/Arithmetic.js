2f555a69391995407a37916383054cdf5f764b7d
sairatabassum <sairatabassum386@gmail.com>
2021-04-16 14:59:58 +0600

    commit

--- Program3/Arithmetic/Arithmetic.js
+++ Program3/Arithmetic/Arithmetic.js
@@ -11,3 +11,12 @@ document.write(6/6+"<br>");
 document.write(12%3+"<br>");
 
 document.write(2**3+"<br>");
+
+// increment decrement
+
+var num1=6;
+var num2=10;
+
+
+document.write(--num1 + " " + num2-- +"<br>")
+document.write(++num1 + " " +  num2++ +"<br>");
