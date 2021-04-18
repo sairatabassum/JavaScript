0b5f9c2e2cfd53b500082bf8e11ab8b9270e7eff
sairatabassum <sairatabassum386@gmail.com>
2021-04-18 20:29:06 +0600

    commit

---
+++ Program8/function.js
@@ -0,0 +1,17 @@
+
+
+function sum(x,y)
+{
+
+    var res=x+y;
+    document.write(res);
+
+}
+
+var x,y;
+
+x=parseInt(prompt("Enter number1: "));
+y=parseInt(prompt("Enter number2: "));
+
+// Calling Function
+sum(x,y);
