53026df7c054cfaa52d95bc11570a01196c0b0e4
sairatabassum <sairatabassum386@gmail.com>
2021-04-18 19:30:31 +0600

    commit

--- Program7/break&continue.js
+++ Program7/break&continue.js
@@ -1,10 +1,23 @@
 
+// Break
 for(var i=1;i<=10;i++){
 
     if (i==5){
+        document.write('<br>');
         break;
     }
 
-    document.write(i,'<br>')
+    document.write(i,'<br>');
+
+}
+
+// Continue
+for(var i=1;i<=10;i++){
+
+    if (i==5){
+        continue;
+    }
+
+    document.write(i,'<br>');
 
 }
