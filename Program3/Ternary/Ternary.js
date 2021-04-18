200bcfe2e4ec98e2590ba44eb567c2b7e9456fc1
sairatabassum <sairatabassum386@gmail.com>
2021-04-18 20:22:25 +0600

    commit

---
+++ Program3/Ternary/Ternary.js
@@ -0,0 +1,10 @@
+
+var number=parseInt(prompt("Enter a number: "));
+
+// var result= number>0? "positive": "negative";
+
+var result= number>0? "positive": number<0? "Negative": "Zero";
+
+
+document.write(result,'<br>');
+
