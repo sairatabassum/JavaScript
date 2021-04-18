e2163775861d88c673873036c2f0e5331c4e6e31
sairatabassum <sairatabassum386@gmail.com>
2021-04-18 21:50:15 +0600

    commit

--- Program9/Array.js
+++ Program9/Array.js
@@ -1,1 +1,37 @@
 
+// Array declaration
+var num= new Array(5);
+
+var lang=["C","C++","Python","Java"];
+
+// Array initialization
+
+lang.push("Ruby");
+
+num[0]=1;
+
+num[1]=2;
+
+num[2]=3;
+
+num[3]=4;
+
+num[4]=5;
+
+// Array print
+document.write(num,'<br>');
+document.write(lang,'<br>')
+
+for(var i=0;i<num.length;i++){
+
+    document.write(num[i],'<br>');
+}
+
+
+// Remove element
+
+lang.pop();
+document.write('<br>',lang);
+
+// Two Array Concat
+
